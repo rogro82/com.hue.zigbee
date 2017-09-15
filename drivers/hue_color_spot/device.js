72d681389896c24cd950bb33fd591fb52d672c22
Rob Groenendijk <rogro82@gmail.com>
2017-09-15 20:29:53 +0200

    Added LCT003 (color spot)

---
+++ drivers/hue_color_spot/device.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const Homey = require('homey');
+const ZigBeeLightDevice = require('homey-meshdriver').ZigBeeLightDevice;
+
+class HueColorZigBee extends ZigBeeLightDevice {
+}
+
+module.exports = HueColorZigBee;
